bd4928276b7f6ffb17e48cd128bb1fd24f9bfa6b
yoav <yoav.m@tenengroup.com>
2020-05-24 21:55:58 +0300

    get stock data

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from robinhood_util import get_data, get_client
+
 if __name__ == "__main__":
-    print("branch")
+    print("start")
+
+    rh_client = get_client()
+
+    interesting_stocks = ["NVDA", "MSFT", "SQQQ", "SDOW"]
+    interesting_stocks_processed = {}
+
+    for stock in interesting_stocks:
+        current_data = get_data(rh_client, stock)
+        print(f"{stock}'s asking price is {current_data['ask_price']}")
+        interesting_stocks_processed[stock] = current_data
+
+    # something cool
     print("done")
