f20dd7521ae3897657269abe1cc2f08454d6ff0b
yoav <yoav.m@tenengroup.com>
2020-05-24 21:56:46 +0300

    get stock data fix

---
+++ robinhood_util.py
@@ -0,0 +1,18 @@
+from pyrh import Robinhood
+from os import environ
+
+
+def get_client():
+    x = Robinhood()
+    x.client_id = environ.get("RH_CLIENT_ID")
+    x.refresh_token = environ.get("RH_REFRESH_TOKEN")
+    x.device_token = environ.get("RH_DEVICE_TOKEN")
+    x.username = environ.get("RH_USERNAME")
+    x.password = environ.get("RH_PASSWORD")
+    x.login(x.username, x.password)
+
+    return x
+
+
+def get_data(client, symbol):
+    return client.get_quote(symbol)
